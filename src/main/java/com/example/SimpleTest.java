232b5efc88814a7b8112e6f70f530cd13b6ed14e
EvgenPetrovich8 <evgenpetrovich8@gmail.com>
2020-04-25 21:52:52 +0300

    add project study-automation

---
+++ src/main/java/com/example/SimpleTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+
+    @Test
+    public void  calculqationTest(){
+
+        System.out.println("simple test");
+    }
+}
---
+++ src/test/java/com/example/SimpleTest.java
@@ -0,0 +1,14 @@
+package com.example;
+
+
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+
+    @Test
+    public void  calculqationTest(){
+
+        System.out.println("simple test");
+    }
+}
